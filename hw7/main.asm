2839b87684c061cb6e878f339df8c67bcc8fc709
Alex Ryndin <me@alexryndin.me>
2021-12-27 13:24:10 +0300

    hw7: non-recursive m

--- hw7/main.asm
+++ hw7/main.asm
@@ -1,8 +1,8 @@
-    bits 64
-    extern malloc, puts, printf, fflush, abort, free
-    global main
+	bits 64
+	extern malloc, puts, printf, fflush, abort, free
+	global main
 
-    section   .data
+	section   .data
 empty_str: db 0x0
 int_format: db "%ld ", 0x0
 ;; dq -- 8 byte constant
@@ -10,175 +10,176 @@ data: dq 4, 8, 15, 16, 23, 42
 ;; data_length = 6
 data_length: equ ($-data) / 8
 
-    section   .text
+	section   .text
 ;;; print_int proc
 print_int:
-    mov rsi, rdi
-    mov rdi, int_format
-    xor rax, rax
-    call printf
+	mov rsi, rdi
+	mov rdi, int_format
+	xor rax, rax
+	call printf
 
-    xor rdi, rdi
-    call fflush
+	xor rdi, rdi
+	call fflush
 
-    ret
+	ret
 
 ;;; p proc
 p:
-    mov rax, rdi
-    and rax, 1
-    ret
+	mov rax, rdi
+	and rax, 1
+	ret
 
 ;;; add_element proc
 add_element:
-    push rbp
-    push rbx
+	push rbp
+	push rbx
 
-    mov rbp, rdi
-    mov rbx, rsi
+	mov rbp, rdi
+	mov rbx, rsi
 
-    mov rdi, 16
-    call malloc
-    test rax, rax
-    jz abort
+	mov rdi, 16
+	call malloc
+	test rax, rax
+	jz abort
 
-    mov [rax], rbp
-    mov [rax + 8], rbx
+	mov [rax], rbp
+	mov [rax + 8], rbx
 
-    pop rbx
-    pop rbp
+	pop rbx
+	pop rbp
 
-    ret
+	ret
 
 ;;; free_list proc
 free_list:
-    test rdi, rdi
-    jz out_free_list
+	test rdi, rdi
+	jz out_free_list
 
-    push rbp
-    push rbx
+	push rbp
+	push rbx
 
-    mov rbx, [rdi + 8]
+	mov rbx, [rdi + 8]
 
-    call free
+	call free
 
-    mov rdi, rbx
-    call free_list
+	mov rdi, rbx
+	call free_list
 
-    pop rbx
-    pop rbp
+	pop rbx
+	pop rbp
 
 out_free_list:
-    ret
+	ret
 
 ;;; m proc
 m:
-    test rdi, rdi
-    jz outm
 
-    push rbp
-    push rbx
+	push rbp
+	push rbx
 
-    mov rbx, rdi
-    mov rbp, rsi
+	mov rbx, rdi
+	mov rbp, rsi
+m_loop:
+	test rbx, rbx
+	jz outm
 
-    mov rdi, [rdi]
-    call rsi
+	mov rdi, [rbx]
+	call rsi
 
-    mov rdi, [rbx + 8]
-    mov rsi, rbp
-    call m
-
-    pop rbx
-    pop rbp
+	mov rbx, [rbx + 8]
+	mov rsi, rbp
+	jmp m_loop
 
 outm:
-    ret
+	pop rbx
+	pop rbp
+
+	ret
 
 ;;; f proc
 f:
-    mov rax, rsi
+	mov rax, rsi
 
-    test rdi, rdi
-    jz outf
+	test rdi, rdi
+	jz outf
 
-    push rbx
-    push r12
-    push r13
+	push rbx
+	push r12
+	push r13
 
-    mov rbx, rdi
-    mov r12, rsi
-    mov r13, rdx
+	mov rbx, rdi
+	mov r12, rsi
+	mov r13, rdx
 
-    mov rdi, [rdi]
-    call rdx
-    test rax, rax
-    jz z
+	mov rdi, [rdi]
+	call rdx
+	test rax, rax
+	jz z
 
-    mov rdi, [rbx]
-    mov rsi, r12
-    call add_element
-    mov rsi, rax
-    jmp ff
+	mov rdi, [rbx]
+	mov rsi, r12
+	call add_element
+	mov rsi, rax
+	jmp ff
 
 z:
-    mov rsi, r12
+	mov rsi, r12
 
 ff:
-    mov rdi, [rbx + 8]
-    mov rdx, r13
-    call f
+	mov rdi, [rbx + 8]
+	mov rdx, r13
+	call f
 
-    pop r13
-    pop r12
-    pop rbx
+	pop r13
+	pop r12
+	pop rbx
 
 outf:
-    ret
+	ret
 
 ;;; main proc
 main:
-    push rbx
-    push r12
+	push rbx
+	push r12
 
-    xor rax, rax
-    mov rbx, data_length
+	xor rax, rax
+	mov rbx, data_length
 adding_loop:
-    mov rdi, [data - 8 + rbx * 8]
-    mov rsi, rax
-    call add_element
-    dec rbx
-    jnz adding_loop
+	mov rdi, [data - 8 + rbx * 8]
+	mov rsi, rax
+	call add_element
+	dec rbx
+	jnz adding_loop
 
-    mov rbx, rax
+	mov rbx, rax
 
-    mov rdi, rax
-    mov rsi, print_int
-    call m
+	mov rdi, rax
+	mov rsi, print_int
+	call m
 
-    mov rdi, empty_str
-    call puts
+	mov rdi, empty_str
+	call puts
 
-    mov rdx, p
-    xor rsi, rsi
-    mov rdi, rbx
-    call f
-    mov r12, rax
+	mov rdx, p
+	xor rsi, rsi
+	mov rdi, rbx
+	call f
+	mov r12, rax
 
-    mov rdi, rax
-    mov rsi, print_int
-    call m
+	mov rdi, rax
+	mov rsi, print_int
+	call m
 
-    mov rdi, empty_str
-    call puts
+	mov rdi, empty_str
+	call puts
 
-    mov rdi, rbx
-    call free_list
-    mov rdi, r12
-    call free_list
+	mov rdi, rbx
+	call free_list
+	mov rdi, r12
+	call free_list
 
-    pop r12
+	pop r12
 	pop rbx
 
-    xor rax, rax
-    ret
+	xor rax, rax
+	ret
